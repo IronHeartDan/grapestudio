b95c18cfee9cd850b661336c9f26c228788d4291
Danish Khan <danishkhan.dk2.dk16@gmail.com>
2021-07-01 01:37:28 +0530

    s_offer new change init

--- src/App.js
+++ src/App.js
@@ -31,77 +31,77 @@ function App() {
     document.querySelector(".app").scrollTop = 0;
   };
 
-  useEffect(() => {
-    console.log("UseEffect");
-    const magic = new IntersectionObserver((entries) => {
-      entries.forEach((entry, index) => {
-        let animType = "none";
-        switch (index) {
-          case 0:
-            animType = "PopBottom";
-            break;
-          case 1:
-            animType = "PopBottom";
-            break;
-          default:
-            animType = "PopBottom";
-        }
-        if (entry.intersectionRatio > 0) {
-          entry.target.style.animation = `${animType} ease 1s 0s`;
-          entry.target.style.opacity = "1";
-        } else {
-          entry.target.style.animation = "none";
-        }
-      });
-    });
-    // For Anim
-    let elements = document.querySelectorAll(".anim");
-    elements.forEach((element) => {
-      magic.observe(element);
-    });
+  // useEffect(() => {
+  //   console.log("UseEffect");
+  //   const magic = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry, index) => {
+  //       let animType = "none";
+  //       switch (index) {
+  //         case 0:
+  //           animType = "PopBottom";
+  //           break;
+  //         case 1:
+  //           animType = "PopBottom";
+  //           break;
+  //         default:
+  //           animType = "PopBottom";
+  //       }
+  //       if (entry.intersectionRatio > 0) {
+  //         entry.target.style.animation = `${animType} ease 1s 0s`;
+  //         entry.target.style.opacity = "1";
+  //       } else {
+  //         entry.target.style.animation = "none";
+  //       }
+  //     });
+  //   });
+  //   // For Anim
+  //   let elements = document.querySelectorAll(".anim");
+  //   elements.forEach((element) => {
+  //     magic.observe(element);
+  //   });
 
-    const offerMagic = new IntersectionObserver((entries) => {
-      console.log("djbfsdj");
-      if (entries[0].intersectionRatio > 0) {
-        let offer_anim_cards = document.querySelectorAll(".offer_anim_card");
-        offer_anim_cards.forEach((card, ci) => {
-          switch (ci) {
-            case 0:
-              card.style.animation =
-                " magix 3s 0s forwards ease, one 1s 18s forwards ease,card 0s 18s forwards ease";
-              break;
-            case 1:
-              card.style.animation =
-                "magix 5s 2s forwards ease, two 1s 18.5s forwards ease,card 0s 18s forwards ease";
-              break;
-            case 2:
-              card.style.animation =
-                "magix 5s 5s forwards ease, three 1s 19s forwards ease,card 0s 18s forwards ease";
-              break;
-            case 3:
-              card.style.animation =
-                " magix 5s 8s forwards ease, four 1s 19.5s forwards ease,card 0s 18s forwards ease";
-              break;
-            case 4:
-              card.style.animation =
-                " magix 5s 11s forwards ease, five 1s 20s forwards ease,card 0s 18s forwards ease";
-              break;
-            case 5:
-              card.style.animation =
-                "magix 5s 14s forwards ease, six 0.5s 20.5s forwards ease,card 0s 18s forwards ease";
-              break;
+  //   const offerMagic = new IntersectionObserver((entries) => {
+  //     console.log("djbfsdj");
+  //     if (entries[0].intersectionRatio > 0) {
+  //       let offer_anim_cards = document.querySelectorAll(".offer_anim_card");
+  //       offer_anim_cards.forEach((card, ci) => {
+  //         switch (ci) {
+  //           case 0:
+  //             card.style.animation =
+  //               " magix 3s 0s forwards ease, one 1s 18s forwards ease,card 0s 18s forwards ease";
+  //             break;
+  //           case 1:
+  //             card.style.animation =
+  //               "magix 5s 2s forwards ease, two 1s 18.5s forwards ease,card 0s 18s forwards ease";
+  //             break;
+  //           case 2:
+  //             card.style.animation =
+  //               "magix 5s 5s forwards ease, three 1s 19s forwards ease,card 0s 18s forwards ease";
+  //             break;
+  //           case 3:
+  //             card.style.animation =
+  //               " magix 5s 8s forwards ease, four 1s 19.5s forwards ease,card 0s 18s forwards ease";
+  //             break;
+  //           case 4:
+  //             card.style.animation =
+  //               " magix 5s 11s forwards ease, five 1s 20s forwards ease,card 0s 18s forwards ease";
+  //             break;
+  //           case 5:
+  //             card.style.animation =
+  //               "magix 5s 14s forwards ease, six 0.5s 20.5s forwards ease,card 0s 18s forwards ease";
+  //             break;
 
-            default:
-              break;
-          }
-        });
-      }
-    });
+  //           default:
+  //             break;
+  //         }
+  //       });
+  //     }
+  //   });
 
-    // For anim_offer
-    let oE = document.querySelector(".anim_offer");
-    offerMagic.observe(oE);
-  });
+  //   // For anim_offer
+  //   let oE = document.querySelector(".anim_offer");
+  //   offerMagic.observe(oE);
+  // });
 
   return (
     <div className="app" onScroll={onScroll}>
@@ -198,165 +198,27 @@ function App() {
       {/* s_offer */}
 
       <section className="s_offer">
-        <div className="anim_offer" id="anim_offer_con_one">
-          <div>
-            <div className="offer_anim_card" onMouseOver={() => setOffer(1)}>
-              <h6>Mobile App development</h6>
-            </div>
-            <div className="offer_anim_card" onMouseOver={() => setOffer(2)}>
-              <h6>Website development</h6>
-            </div>
-            <div className="offer_anim_card" onMouseOver={() => setOffer(3)}>
-              <h6>Web/ Mobile App design</h6>
-            </div>
-            <div className="offer_anim_card" onMouseOver={() => setOffer(4)}>
-              <h6>Graphic designing</h6>
-            </div>
-            <div className="offer_anim_card" onMouseOver={() => setOffer(5)}>
-              <h6>Five</h6>
-            </div>
-            <div className="offer_anim_card" onMouseOver={() => setOffer(6)}>
-              <h6>Six</h6>
-            </div>
-          </div>
+        <div className="offer_anim_card" id="0">
+          <h1>One</h1>
         </div>
-        <div>
-          <div>
-            {(() => {
-              switch (offersState) {
-                case 1:
-                  return (
-                    <>
-                      <h1>Mobile App development</h1>
-                      <ul>
-                        <li> Highly proficient in Android app development.</li>
-                        <li>
-                          Hiring passionate and dedicated developers to produce
-                          the best results.
-                        </li>
-                        <li>
-                          Showcasing innovative and enthusiastic approach while
-                          building android apps
-                        </li>
-                        <li>
-                          Working with existing and updated technologies for
-                          secure and sturdy app development.
-                        </li>
-                        <li>
-                          Providing steady and satisfactory result to our
-                          clients.
-                        </li>
-                        <li>Delivering with exceptional punctuality.</li>
-                      </ul>
-                    </>
-                  );
-                case 2:
-                  return (
-                    <>
-                      <h1>Website development</h1>
-                      <ul>
-                        <li> Highly proficient in website development.</li>
-                        <li>
-                          Conducting innovative tactic while building
-                          static/dynamic websites.
-                        </li>
-                        <li>
-                          Hiring passionate and dedicated individuals to ensure
-                          smooth development.
-                        </li>
-                        <li>
-                          Developing dynamic/static websites from scratch.
-                        </li>
-                        <li>Providing better user interactive interface.</li>
-                        <li>
-                          Constructing wide sets of websites ranging from
-                          (Personal, Photo sharing, writer/author, informative,
-                          ecommerce).
-                        </li>
-                      </ul>
-                    </>
-                  );
-                case 3:
-                  return (
-                    <>
-                      <h1>Web/ Mobile App design</h1>
-                      <ul>
-                        <li>Highly proficient in web/mobile app designing.</li>
-                        <li>
-                          Offering interactive UI & UX designs for your Mobile
-                          applications/Websites.
-                        </li>
-                        <li>
-                          Following professional procedures while designing your
-                          respective web/Mobile app.
-                        </li>
-                        <li>Ensuring timely delivery of orders.</li>
-                        <li>
-                          Using creative tools to maximize user interactions.
-                        </li>
-                        <li> Hiring professional UI/UX designers.</li>
-                      </ul>
-                    </>
-                  );
-                case 4:
-                  return (
-                    <>
-                      <h1>Graphic designing</h1>
-                      <ul>
-                        <li>Highly proficient in Graphic designing.</li>
-                        <li>
-                          <strong>Logo designing:-</strong> ➔ A team of
-                          experienced logo designers providing services from
-                          basic and complex logo designs for small scale
-                          businesses to big enterprising corporations.
-                        </li>
-                        <li>
-                          <strong>Business cards designing:-</strong> ➔ Adding
-                          life to your business cards by using creative and
-                          professional designing methods while developing them.
-                        </li>
-                        <li>
-                          <strong>Social media posts designing:- </strong>{" "}
-                          ➔Providing interactive post designs for engaging more
-                          audiences.
-                        </li>
-                        <li>
-                          <strong>Business Flyer designing:-</strong> ➔ Creating
-                          your own professional business flyer designs allowing
-                          you to express the story of your case. Whether it’s
-                          for business, promoting, fundraisers etc
-                        </li>
-                        <li>
-                          Ensuring timely delivery of each and every design.
-                        </li>
-                      </ul>
-                    </>
-                  );
-                case 5:
-                  return (
-                    <>
-                      <h1>Five</h1>
-                      <ul>
-                        <li>Five</li>
-                      </ul>
-                    </>
-                  );
-                case 6:
-                  return (
-                    <>
-                      <h1>Six</h1>
-                      <ul>
-                        <li> Six</li>
-                      </ul>
-                    </>
-                  );
-
-                default:
-                  return <></>;
-              }
-            })()}
-          </div>
+        <div className="offer_anim_card" id="1">
+          <h1>Two</h1>
         </div>
+        <div className="offer_anim_card" id="2">
+          <h1>Three</h1>
+        </div>
+        <div className="offer_anim_card" id="3">
+          <h1>Four</h1>
+        </div>
+        <div className="offer_anim_card" id="4">
+          <h1>Five</h1>
+        </div>
+        <div className="offer_anim_card" id="5">
+          <h1>Six</h1>
+        </div>
+        {/* <div className="offer_anim_card" id="6">
+          <h1>Seven</h1>
+        </div> */}
       </section>
 
       {/* s_offer */}
@@ -529,11 +391,13 @@ function App() {
         </div>
       </section>
       <footer>
-        <a href="/">Privacy Policy</a>
-        <span>|</span>
-        <a href="/">Disclaimer</a>
-        <span>|</span>
-        <a href="/">Terms And Conditions</a>
+        <div>
+          <a href="/">Privacy Policy</a>
+          <span>|</span>
+          <a href="/">Disclaimer</a>
+          <span>|</span>
+          <a href="/">Terms And Conditions</a>
+        </div>
         <p>Copyright © Grape Studio Enterprise</p>
       </footer>
     </div>
